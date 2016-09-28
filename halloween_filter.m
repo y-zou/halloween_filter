ead6281063a4e763b754d571f857047b5e699069
Yiyue Zou <yxz5480@win.psu.edu>
2016-09-28 10:01:35 -0400

    Change script to a function

--- halloween_filter.m
+++ halloween_filter.m
@@ -1,29 +1,31 @@
-im = imread('Magnolia.png');
+function f = halloween_filter(picture)
+    im = imread(picture);
 
 
-figure(1);
+    f = figure(1);
 
-subplot(2,1,1)
-imagesc(im);
+    subplot(1,2,1)
+    imagesc(im);
 
-hold off;
-
-
-im = double(im);
-
-gim = (im(:,:,1)+im(:,:,2)+im(:,:,3))/3;
-
-Ix_filter=[-1,0,1]/2;
-Iy_filter=[-1;0;1]/2;
+    hold off;
 
 
-Ix = imfilter(gim,Ix_filter);
-Iy = imfilter(gim,Iy_filter);
-gradient_magn = sqrt(Ix.^2 + Iy.^2);
+    im = double(im);
+
+    gim = (im(:,:,1)+im(:,:,2)+im(:,:,3))/3;
+
+    Ix_filter=[-1,0,1]/2;
+    Iy_filter=[-1;0;1]/2;
 
 
-subplot(2,1,2)
-imagesc(gradient_magn)
+    Ix = imfilter(gim,Ix_filter);
+    Iy = imfilter(gim,Iy_filter);
+    gradient_magn = sqrt(Ix.^2 + Iy.^2);
 
-colormap(hot)
 
+    subplot(1,2,2)
+    imagesc(gradient_magn)
+
+    colormap(hot)
+
+end
